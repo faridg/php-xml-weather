a072593842883c02bf545ffba014bfb016db95ca
faridg <farid.gassanov@gmail.com>
2018-02-16 20:07:49 -0500

    now with heading, sky icon and list items

--- celsius.php
+++ celsius.php
@@ -1,19 +1,22 @@
 <?php
 
-	$city = 'New York';
+	$location = 'New York';
 	$units = 'metric';
 	$mode = 'xml';
 	$key = '7154dddfd3b6fde40800ed15340d8895';
 	
-	$forecast = 'http://api.openweathermap.org/data/2.5/weather?'.'q='.$city.'&units='.$units.'&mode='.$mode.'&APPID='.$key;
+	$forecast = 'http://api.openweathermap.org/data/2.5/weather?'.'q='.$location.'&units='.$units.'&mode='.$mode.'&APPID='.$key;
 	
 	$xml = simplexml_load_file($forecast) or die ('wanderror');
 	
-	echo $xml->city['name'];
-	echo $xml->weather['value'];
-	echo $xml->temperature['value'];
-	echo $xml->temperature['unit'];
-	echo $xml->wind->speed['name'];
-	echo $xml->humidity['value'];
+	$sky ='http://api.openweathermap.org/img/w/'.$xml->weather['icon'].'.png';
+	
+	echo '<h1> Today in '.$xml->city['name'];
+	echo ' it\'s '.$xml->weather['value'].'<img src='.$sky.'></h1>';
+	echo '<ul><li>Temperature: '.$xml->temperature['value'];
+	echo '&deg;';
+	echo '<li>Wind: '.$xml->wind->speed['name'];
+	echo '<li>Humidity: '.$xml->humidity['value'];
+	echo '&#37;';
 
 ?>
